20608a2ced6c9098fe6e6438b06c870b11fc103c
Paulo Henrique <phenrique4570@gmail.comgit config --global user.email>
2020-08-30 10:55:11 -0300

    interação front e back desafio react

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
-import React from "react";
+import React, {useEffect, useState } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'Repositório desafio',
+      url: 'https://github.com/Paulo-101/desafio2bootcamp',
+      techs:['NodeJs', 'ReactJS']
+    })
+
+    setRepositories([...repositories, response.data ])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    setRepositories(repositories.filter(
+      repository => repository.id != id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+          {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+          <button onClick={() => handleRemoveRepository(repository.id)}>
+            Remover Repositório
           </button>
         </li>
+        ))}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button onClick={handleAddRepository}>Adicionar Repositório</button>
     </div>
   );
 }
